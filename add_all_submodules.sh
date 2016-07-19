b037d958b61443725fe7f4344608efb90972aba1
colineberl-coe <ceberlcoe@sovrn.com>
2016-07-19 10:46:25 -0600

    Added vim_makes_my_life_easy

--- add_all_submodules.sh
+++ add_all_submodules.sh
@@ -5,6 +5,7 @@ git clone  https://github.com/tomtom/tlib_vim ~/.vim/bundle/tlib_vim
 git clone  https://github.com/MarcWeber/vim-addon-mw-utils ~/.vim/bundle/vim-addon-mw-utils 
 git clone  https://github.com/garbas/vim-snipmate ~/.vim/bundle/vim-snipmate
 git clone  https://github.com/honza/vim-snippets ~/.vim/bundle/vim-snippets
-git clone  https://github.com/SirVer/ultisnips ~/.vim/bundle/ultisnips
+# git clone  https://github.com/SirVer/ultisnips ~/.vim/bundle/ultisnips
 git clone  https://github.com/tpope/vim-commentary ~/.vim/bundle/commentary
 git clone https://github.com/easymotion/vim-easymotion ~/.vim/bundle/easymotion
+git clone https://github.com/ajh17/VimCompletesMe ~/.vim/bundle/VimCompletesMe
