1d94173a6fe06c0fa0b4dc5cd6d42588667f83d3
ScouserInTrousers <scouserintrousers@no-reply.github.com>
2017-02-23 15:18:40 -0700

    Remove commented old code

--- add_all_submodules.sh
+++ add_all_submodules.sh
@@ -22,15 +22,3 @@ git submodule init
 git submodule update
 git submodule foreach git submodule init
 git submodule foreach git submodule update
-
-# mkdir ~/.vim/autoload
-# git clone https://github.com/tpope/vim-pathogen ~/.vim/autoload/pathogen
-# cp ~/.vim/autoload/pathogen/autoload/pathogen.vim ~/.vim/autoload/pathogen.vim
-# git clone https://github.com/tomtom/tlib_vim ~/.vim/bundle/tlib_vim
-# git clone https://github.com/MarcWeber/vim-addon-mw-utils ~/.vim/bundle/vim-addon-mw-utils 
-# git clone https://github.com/garbas/vim-snipmate ~/.vim/bundle/vim-snipmate
-# git clone https://github.com/honza/vim-snippets ~/.vim/bundle/vim-snippets
-# git clone https://github.com/sirver/ultisnips ~/.vim/bundle/ultisnips
-# git clone https://github.com/tpope/vim-commentary ~/.vim/bundle/commentary
-# git clone https://github.com/easymotion/vim-easymotion ~/.vim/bundle/easymotion
-# git clone https://github.com/ajh17/VimCompletesMe ~/.vim/bundle/VimCompletesMe
